50775bcc1237717f751155a8b2d2ed8fd927babb
eliericksen1-blip <eliericksen1@gmail.com>
2026-09-18 12:57:45 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,13 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
+    createPlatform(
+      -50,
+      canvas.height - 10,
+      canvas.width + 100,
+      200,
+      "rgb(118, 0, 233)",
+    ); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -29,22 +35,22 @@ $(function () {
     // TODO 1 - Enable the Grid
     // toggleGrid();
 
-
     // TODO 2 - Create Platforms
-
-
-
+    createPlatform(300, 650, 100, 10);
+    createPlatform(250, 220, 100, 50);
+    createPlatform(450, 350, 100, 50);
+    createPlatform(350, 450, 100, 50);
+    createPlatform(150, 550, 100, 50);
+    createPlatform(350, 750, 100, 50);
+    createPlatform(50, 150, 100, 50);
 
     // TODO 3 - Create Collectables
-
-
-
-    
+    createCollectable("steve", 250, 180);
+    createCollectable("diamond", 300, 600);
+    createCollectable("max",450, 300)
     // TODO 4 - Create Cannons
-
-
-    
-    
+    createCannon("top", 350, 1000);
+    createCannon("right", 300, 1000);
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
